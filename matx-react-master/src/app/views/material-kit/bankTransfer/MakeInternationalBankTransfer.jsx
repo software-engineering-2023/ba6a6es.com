54fb6fc5511ea3f45b37349fe1580fba9189293f
Hauna9 <85716100+Hauna9@users.noreply.github.com>
2023-05-26 23:11:24 +0300

    Templates for Bank Transfer
    Made the pages for Bank Transfer, need to actually edit the attributes inside and match the extra stuff per type of bank trasnfer

---
+++ matx-react-master/src/app/views/material-kit/bankTransfer/MakeInternationalBankTransfer.jsx
@@ -0,0 +1,33 @@
+import { Stack } from "@mui/material";
+import { Box, styled } from "@mui/system";
+import { Breadcrumb, SimpleCard } from "app/components";
+import MakeInternationalBankTransferForm from "app/views/material-kit/forms/MakeInternationalBankTransferForm";
+
+const Container = styled("div")(({ theme }) => ({
+  margin: "30px",
+  [theme.breakpoints.down("sm")]: { margin: "16px" },
+  "& .breadcrumb": {
+    marginBottom: "30px",
+    [theme.breakpoints.down("sm")]: { marginBottom: "16px" },
+  },
+}));
+
+const MakeInternationalBankTransfer = () => {
+  return (
+    <Container>
+      <Box className="breadcrumb">
+        <Breadcrumb routeSegments={[{ name: "Material", path: "/material" }, { name: "Form" }]} />
+      </Box>
+
+      <Stack spacing={3}>
+        <SimpleCard title="Make International Bank Transfer Form">
+          <MakeInternationalBankTransferForm/>
+        </SimpleCard>
+
+        
+      </Stack>
+    </Container>
+  );
+};
+
+export default MakeInternationalBankTransfer;
