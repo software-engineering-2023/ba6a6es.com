5a800e34a58f38f5df9afb65d03388d271d4e377
Hauna9 <85716100+Hauna9@users.noreply.github.com>
2023-05-29 22:08:28 +0300

    Handled buttons in open and close account req in banker

---
+++ matx-react-master/src/app/views/material-kit/banker/OpenAccountRequests.jsx
@@ -0,0 +1,30 @@
+import { Box, styled } from "@mui/material";
+import { Breadcrumb, SimpleCard } from "app/components";
+import OpenBankAccountRequestsTable from "app/views/material-kit/tables/OpenBankAccountRequestsTable";
+
+const Container = styled("div")(({ theme }) => ({
+  margin: "30px",
+  [theme.breakpoints.down("sm")]: { margin: "16px" },
+  "& .breadcrumb": {
+    marginBottom: "30px",
+    [theme.breakpoints.down("sm")]: { marginBottom: "16px" },
+  },
+}));
+
+const OpenAccountRequests = () => {
+  return (
+    <Container>
+      <Box className="breadcrumb">
+        <Breadcrumb routeSegments={[{ name: "Material", path: "/material" }, { name: "Table" }]} />
+      </Box>
+
+      <SimpleCard title="Simple Table">
+        <OpenBankAccountRequestsTable />
+      </SimpleCard>
+
+      
+    </Container>
+  );
+};
+
+export default OpenAccountRequests;
