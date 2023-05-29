f4c927ad2382b123cb128310042bcf0c529982ed
Zeyadptd <zeyad.habashy02@gmail.com>
2023-05-29 17:59:37 +0300

    made domestic bank transfer form nicer

--- matx-react-master/src/app/views/material-kit/forms/MakeDomesticBankTransferForm.jsx
+++ matx-react-master/src/app/views/material-kit/forms/MakeDomesticBankTransferForm.jsx
@@ -1,148 +1,124 @@
-import { Button, Grid, Icon, styled } from '@mui/material';
-import { Span } from 'app/components/Typography';
 import { useState } from 'react';
-import { TextValidator } from 'react-material-ui-form-validator';
-import BankAccountDetails from '../bank/BankAccountDetails';
-import SelectedMenu from 'app/components/Common/Menus/SelectedMenu';
-
-const TextField = styled(TextValidator)(() => ({
-  width: '100%',
-  marginBottom: '16px',
-}));
-
-const Form = styled('form')(() => ({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-}));
-
-const Label = styled('label')(() => ({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  marginBottom: '16px',
-}));
-
-const Input = styled('input')(() => ({
-  padding: '8px',
-  borderRadius: '4px',
-  border: '1px solid #ccc',
-  width: '100%',
-}));
-
-const ButtonWrapper = styled('div')(() => ({
-  marginTop: '16px',
-}));
-
-const ErrorMessage = styled('div')(() => ({
-  color: 'red',
-  marginTop: '16px',
-}));
+import { Box, Button, Grid, TextField, Typography } from '@mui/material';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const MakeDomesticBankTransferForm = () => {
-  const [account, setAccount] = useState('');
-  const [sent, setSent] = useState('');
-  const [amount, setAmount] = useState('');
-  const [pin, setPin] = useState('');
   const [balance, setBalance] = useState(1000);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const handleAccountKeyPress = (event) => {
-    if (!/[0-9]/.test(event.key)) {
-      event.preventDefault();
-    }
-  };
-
-  const handleSentKeyPress = (event) => {
-    if (!/[0-9]/.test(event.key)) {
-      event.preventDefault();
-    }
-  };
-
-  const handleAmountKeyPress = (event) => {
-    if (!/[0-9]/.test(event.key)) {
-      event.preventDefault();
-    }
-  };
-
-  const handlePinKeyPress = (event) => {
-    if (!/[0-9]/.test(event.key)) {
-      event.preventDefault();
-    }
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (account === '' || sent === '' || amount === '' || pin === '') {
-      setErrorMessage('Please fill in all fields.');
-    } else if (isNaN(amount)) {
-      setErrorMessage('Please enter a valid amount.');
-    } else if (parseInt(amount) > balance) {
-      setErrorMessage('Insufficient balance.');
-    } else {
-      console.log('Account:', account);
-      console.log('Sent:', sent);
-      console.log('Amount:', amount);
-      console.log('Pin:', pin);
-      setBalance(balance - parseInt(amount));
-      setAccount('');
-      setSent('');
-      setAmount('');
-      setPin('');
+  const formik = useFormik({
+    initialValues: {
+      account: '',
+      sent: '',
+      amount: '',
+      pin: '',
+    },
+    validationSchema: Yup.object({
+      account: Yup.string()
+        .required('Required')
+        .matches(/^[0-9]+$/, 'Must be a number'),
+      sent: Yup.string()
+        .required('Required')
+        .matches(/^[0-9]+$/, 'Must be a number'),
+      amount: Yup.number()
+        .required('Required')
+        .positive('Must be a positive number')
+        .integer('Must be an integer')
+        .test('balance', 'Insufficient balance', (value) => value <= balance),
+      pin: Yup.string()
+        .required('Required')
+        .matches(/^[0-9]+$/, 'Must be a number'),
+    }),
+    onSubmit: (values) => {
+      console.log('Account:', values.account);
+      console.log('Sent:', values.sent);
+      console.log('Amount:', values.amount);
+      console.log('Pin:', values.pin);
+      setBalance(balance - parseInt(values.amount));
       setErrorMessage('');
-    }
-  };
+      formik.resetForm();
+    },
+  });
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Label>
-        <Span>Sender Account Number:</Span>
-        <Input
-          type="text"
-          pattern="[0-9]*"
-          value={account}
-          onChange={(e) => setAccount(e.target.value)}
-          onKeyPress={handleAccountKeyPress}
-        />
-      </Label>
-      <Label>
-        <Span>Receiver Account Number:</Span>
-        <Input
-          type="text"
-          pattern="[0-9]*"
-          value={sent}
-          onChange={(e) => setSent(e.target.value)}
-          onKeyPress={handleSentKeyPress}
-        />
-      </Label>
-      <Label>
-        <Span>Amount:</Span>
-        <Input
-          type="text"
-          pattern="[0-9]*"
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-          onKeyPress={handleAmountKeyPress}
-        />
-      </Label>
-      <Label>
-        <Span>Pin:</Span>
-        <Input
-          type="password"
-          pattern="[0-9]*"
-          value={pin}
-          onChange={(e) => setPin(e.target.value)}
-          onKeyPress={handlePinKeyPress}
-        />
-      </Label>
-      <ButtonWrapper>
-        <Button type="submit" variant="contained" color="primary">
-          Confirm
-        </Button>
-      </ButtonWrapper>
-      <div>Balance: {balance}</div>
-      {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
-    </Form>
+    <Box padding={3}>
+      <Grid container spacing={5}>
+        <Grid item sm={6} xs={12}>
+          <form onSubmit={formik.handleSubmit}>
+            <Box marginBottom={2}>
+              <TextField
+                fullWidth
+                type="text"
+                name="account"
+                label="Sender Account Number"
+                variant="outlined"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                value={formik.values.account}
+                error={formik.touched.account && Boolean(formik.errors.account)}
+                helperText={formik.touched.account && formik.errors.account}
+              />
+            </Box>
+            <Box marginBottom={2}>
+              <TextField
+                fullWidth
+                type="text"
+                name="sent"
+                label="Receiver Account Number"
+                variant="outlined"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                value={formik.values.sent}
+                error={formik.touched.sent && Boolean(formik.errors.sent)}
+                helperText={formik.touched.sent && formik.errors.sent}
+              />
+            </Box>
+            <Box marginBottom={2}>
+              <TextField
+                fullWidth
+                type="text"
+                name="amount"
+                label="Amount"
+                variant="outlined"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                value={formik.values.amount}
+                error={formik.touched.amount && Boolean(formik.errors.amount)}
+                helperText={formik.touched.amount && formik.errors.amount}
+              />
+            </Box>
+            <Box marginBottom={2}>
+              <TextField
+                fullWidth
+                type="password"
+                name="pin"
+                label="Pin"
+                variant="outlined"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                value={formik.values.pin}
+                error={formik.touched.pin && Boolean(formik.errors.pin)}
+                helperText={formik.touched.pin && formik.errors.pin}
+              />
+            </Box>
+            <Box marginTop={2}>
+              <Button type="submit" variant="contained" color="primary">
+                Confirm
+              </Button>
+            </Box>
+          </form>
+          <Box marginTop={2}>
+            <Typography>Balance: {balance}</Typography>
+          </Box>
+          {errorMessage && (
+            <Box marginTop={2} color="red">
+              <Typography>{errorMessage}</Typography>
+            </Box>
+          )}
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
